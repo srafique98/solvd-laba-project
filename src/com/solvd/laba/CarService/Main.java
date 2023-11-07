cc6322c67f7d4c59ff03b9a370c8e1b3d0c23100
srafique98 <shanrafique98@gmail.com>
2023-11-07 07:33:01 -0800

    Commenting out main.. stack overflow error

--- src/com/solvd/laba/CarService/Main.java
+++ src/com/solvd/laba/CarService/Main.java
@@ -1,19 +1,54 @@
 package com.solvd.laba.CarService;
 
+import com.solvd.laba.CarService.billing.Cost;
+import com.solvd.laba.CarService.location.Location;
+import com.solvd.laba.CarService.people.Customer;
+import com.solvd.laba.CarService.people.Employee;
+import com.solvd.laba.CarService.serviceManagement.Part;
+import com.solvd.laba.CarService.serviceManagement.Service;
+import com.solvd.laba.CarService.serviceManagement.Vehicle;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        Customer customer1 = new Customer("John Smith", "Doe", new Vehicle[0], new Appointment[0], new Invoice[0], new Service[0]);
-//        Employee employee1 = new Employee("Alice", "Johnson", new Location("City1", "Country1", "Branch1", LocalDate.of(2010, 1, 1), 4.5), "Mechanic", 45000.0);
-//        Vehicle vehicle1 = new Vehicle(LocalDate.of(2022, 1, 1), "SUV", "BrandX", LocalDate.of(2022, 1, 1), false, customer1);
+
+//        Location location1 = new Location("Los Angeles", "USA", "Main Branch");
+//        location1.setEstablishedDate(LocalDate.of(2000, 1, 15));
+//        location1.setRatings(4.5);
 //
-//        System.out.println("Enter the first name of a person: ");
-//        String firstName = scanner.nextLine();
-//        System.out.println("Enter the last name of a person: ");
-//        String lastName = scanner.nextLine();
+//        Customer customer1 = new Customer("John", "Doe", null);
+//        Vehicle[] customerVehicles = new Vehicle[2];
+//        customerVehicles[0] = new Vehicle(LocalDate.of(2020, 3, 15), "Sedan", "Toyota", customer1);
+//        customerVehicles[1] = new Vehicle(LocalDate.of(2019, 8, 20), "SUV", "Honda", customer1);
+//        customer1.setVehicles(customerVehicles);
 //
-//        Person person = new Customer(firstName, lastName, new Vehicle[0], new Appointment[0], new Invoice[0], new Service[0]);
-//        CarServiceUtils.printPersonDetails(person);
-//        scanner.close();
+//        Location employeeLocation = new Location("New York", "USA", "Branch 2");
+//        Employee employee1 = new Employee("Jane", "Smith", employeeLocation, "Mechanic", 60000);
+//
+//        Part servicePart = new Part("12345");
+//        Cost[] serviceCosts = {new Cost(150.0, "USD"), new Cost(200.0, "USD")};
+//        Service service1 = new Service("Oil Change", servicePart, serviceCosts);
+//
+//        CarService carService = new CarService(new Customer[]{customer1}, new Employee[]{employee1}, new Service[]{service1}, new Location[]{location1});
+//        System.out.println("Customers:");
+//        for (Customer customer : carService.getCustomers()) {
+//            System.out.println(customer);
+//        }
+//
+//        System.out.println("\nEmployees:");
+//        for (Employee employee : carService.getEmployees()) {
+//            System.out.println(employee);
+//        }
+//
+//        System.out.println("\nServices:");
+//        for (Service service : carService.getServices()) {
+//            System.out.println(service);
+//        }
+//
+//        System.out.println("\nLocations:");
+//        for (Location location : carService.getLocations()) {
+//            System.out.println(location);
+//        }
     }
 }
